28ef15799e970f9086d95cf324b02da44f765c46
HOHO`` <nov30th@gmail.com>
2022-01-15 06:59:56 +0800

    bug fixes

--- config_flow.py
+++ config_flow.py
@@ -12,8 +12,8 @@ from .hoho_smb import HohoSmbUtil
 
 _LOGGER = logging.getLogger(__name__)
 
-HOHO_DEFAULT_CONF_DICT = {"speaker": "media_player.home_group", "cover_temp_file_path": "config/www",
-                          "cover_temp_file_url": "http://127.0.0.1:8123/local", "file_exts_filter": "mp3;flac",
+HOHO_DEFAULT_CONF_DICT = {"speaker": "media_player.home_group", "cover_temp_file_path": "config/www1",
+                          "cover_temp_file_url": "http://127.0.0.1:8123/local1", "file_exts_filter": "mp3;flac",
                           "fix_ha_media_player": True, "rich_info_support": True,
                           "reset_ordered_index_once_stop_playing": True, "shuffle": True, "netbios": "HOSTNAME",
                           "ip_address": "IP", "username": "USER", "password": "PWD", "root_folder": "FOLDER NAME",
@@ -22,7 +22,7 @@ HOHO_DEFAULT_CONF_DICT = {"speaker": "media_player.home_group", "cover_temp_file
 
 def fillEmptyFields(source: Dict):
     for key in HOHO_DEFAULT_CONF_DICT.keys():
-        if key not in source:
+        if key not in source.keys():
             source[key] = HOHO_DEFAULT_CONF_DICT[key]
 
 
@@ -80,7 +80,7 @@ class HohoChromecastMediaCenterConfigFlow(config_entries.ConfigFlow, domain=DOMA
         fillEmptyFields(user_input)
         if not os.path.exists(user_input[CONF_ALBUM_COVER_TEMP_FILE]):
             os.mkdir(user_input[CONF_ALBUM_COVER_TEMP_FILE])
-        if not os.path.exists():
+        if not os.path.exists(user_input[CONF_ALBUM_COVER_TEMP_FILE]):
             _LOGGER.warning("local path does not exist.")
             return self.async_show_form(step_id="hoho", data_schema=vol.Schema(
                 UserInputUtils.page1(user_input, await self.get_media_players())), errors={"base": "path_not_exist"})
@@ -164,8 +164,8 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_hoho(self, user_input=None):
         self.data.update(user_input.items())
-        if not os.path.exists(user_input[CONF_ALBUM_COVER_TEMP_FILE]):
-            os.mkdir(user_input[CONF_ALBUM_COVER_TEMP_FILE])
+        if not os.path.exists(self.data[CONF_ALBUM_COVER_TEMP_FILE]):
+            os.mkdir(self.data[CONF_ALBUM_COVER_TEMP_FILE])
         if not os.path.exists(self.data[CONF_ALBUM_COVER_TEMP_FILE]):
             _LOGGER.warning("local path does not exist.")
             return self.async_show_form(step_id="hoho", data_schema=vol.Schema(
